4af9b2debd8b882f68e83e8b4944fd7d16d36c8e
the-dragon-born <twofacedrk@gmail.com>
2024-03-27 20:41:14 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+
+setup(
+        name = 'ML-project-1',
+        version = '0.0.1',
+        author = 'dovahkin',
+        author_email = 'twofacedrk@gmail.com',
+        packages = find_packages(),
+        install_requires = ['pandas','numpy','seaborn']
+
+
+
+)
