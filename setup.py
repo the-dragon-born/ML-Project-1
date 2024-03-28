e2e5ea027f1f86a115e9b89b1ca28baa63d8b3e2
the-dragon-born <twofacedrk@gmail.com>
2024-03-28 11:46:21 +0530

    third commit

--- setup.py
+++ setup.py
@@ -1,4 +1,22 @@
 from setuptools import find_packages, setup
+from typing import List
+
+
+hyphen_e = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+
+    '''
+    This function will return the list of requirements
+    '''
+    requirements= []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","")for req in requirements]
+
+        if hyphen_e in requirements:
+            requirements.remove(hyphen_e)
+    return requirements
 
 
 setup(
@@ -7,7 +25,7 @@ setup(
         author = 'dovahkin',
         author_email = 'twofacedrk@gmail.com',
         packages = find_packages(),
-        install_requires = ['pandas','numpy','seaborn']
+        install_requires = get_requirements('requirements.text')
 
 
 
